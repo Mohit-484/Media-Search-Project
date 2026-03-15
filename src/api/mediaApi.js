aaa26276ada067da7bb212a6693e25d75f111d7c
Mohit Selokar <127581929+Mohit-484@users.noreply.github.com>
2026-03-16 01:22:43 +0530

    Update mediaApi.js

--- src/api/mediaApi.js
+++ src/api/mediaApi.js
@@ -4,7 +4,7 @@ const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
 const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
 const TENOR_KEY = import.meta.env.VITE_TENOR_KEY
 
-export async function fetchPhotos(query,per_page=40) {
+export async function fetchPhotos(query,per_page=28) {
    const res = await axios.get('https://api.unsplash.com/search/photos',{
       params:{query,per_page},
       headers:{Authorization:`Client-ID ${UNSPLASH_KEY}`}
