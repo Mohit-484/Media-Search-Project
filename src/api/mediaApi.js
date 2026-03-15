3680cdface441a7ec8770e3bec631372767e6af5
Mohit Selokar <127581929+Mohit-484@users.noreply.github.com>
2026-03-16 01:08:25 +0530

    Update mediaApi.js
    Just changes per page value for better ui.

--- src/api/mediaApi.js
+++ src/api/mediaApi.js
@@ -4,7 +4,7 @@ const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
 const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
 const TENOR_KEY = import.meta.env.VITE_TENOR_KEY
 
-export async function fetchPhotos(query,page=1,per_page=40) {
+export async function fetchPhotos(query,per_page=40) {
    const res = await axios.get('https://api.unsplash.com/search/photos',{
       params:{query,page,per_page},
       headers:{Authorization:`Client-ID ${UNSPLASH_KEY}`}
@@ -14,7 +14,7 @@ export async function fetchPhotos(query,page=1,per_page=40) {
 
 }
 
-export async function fetchVideos(query,per_page=25) {
+export async function fetchVideos(query,per_page=40) {
    const res = await axios.get('https://api.pexels.com/videos/search',{
       params:{query,per_page},
       headers:{Authorization:PEXELS_KEY}
